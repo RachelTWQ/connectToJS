6e9f42e161da7a0aded5a2e0b212b493799d4470
RachelTWQ <racheltang15@hotmail.com>
2019-04-23 18:21:31 +0000

    clear comment

--- findPeopleKnex.js
+++ findPeopleKnex.js
@@ -11,17 +11,6 @@ const knex = require('knex')({
     }
   });
 
-// const settings = require("./settings"); // settings.json
-
-// const client = new pg.Client({
-//     user: settings.user,
-//     password: settings.password,
-//     database: settings.database,
-//     host: settings.hostname,
-//     port: settings.port,
-//     ssl: settings.ssl
-// });
-
 let inputName = process.argv[2];
 
 console.log("Searching ...");
@@ -37,21 +26,6 @@ function findPeople (callback) {
     knex.destroy();
 }); 
 }
-
-// function findPeople(callback) {
-//     client.connect((err) => {
-//         if (err) {
-//             return console.error("Connection Error", err);
-//         }
-//         client.query(`SELECT first_name, last_name, birthdate FROM famous_people WHERE first_name = $1::text`, [inputName], (err, result) => {
-//             if (err) {
-//                 return console.error("error running query", err);
-//             }
-//             callback(result.rows); 
-//             client.end();
-//         });
-//     });
-// }
 
 findPeople(function(output) {
     console.log(`Found ${output.length} person(s) by the name '${inputName}':`);
